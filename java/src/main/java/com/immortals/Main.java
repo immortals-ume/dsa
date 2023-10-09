82130dcb55868769911b195775c268ad78760881
immortals-ume <kaish127799@outlook.com>
2023-10-09 17:25:18 +0530

    Graph And Graph Algorithms Implemented

--- java/src/main/java/com/immortals/Main.java
+++ java/src/main/java/com/immortals/Main.java
@@ -1,7 +1,35 @@
 package com.immortals;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    /**
+     * audio streaming app , playlist play in Random order
+     * [String,String ]
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> strings = new java.util.ArrayList<>(List.of("a", "b", "c", "d"));
+        for (int i = 0; i < 10; i++) {
+            System.out.println(" ");
+            Collections.shuffle(strings);
+            getRandomStrings(strings);
+            strings.forEach(System.out::println);
+        }
+    }
+
+    private static List<String> getRandomStrings(List<String> songs) {
+        Random random = new Random();
+        for (int i = songs.size() - 1; i > 0; i--) {
+            int randomIndex = random.nextInt(i + 1);
+
+            String temp = songs.get(i);
+            songs.set(i, songs.get(randomIndex));
+            songs.set(randomIndex, temp);
+        }
+        return songs;
     }
 }
