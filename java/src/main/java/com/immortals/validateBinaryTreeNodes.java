15ddde306ae2a96eadefc521a91d0466273fd20a
immortals-ume <kaish127799@outlook.com>
2023-10-17 23:55:49 +0530

    Updated main readme.md to various implementations

--- java/src/main/java/com/immortals/validateBinaryTreeNodes.java
+++ java/src/main/java/com/immortals/validateBinaryTreeNodes.java
@@ -1,60 +1,56 @@
 package com.immortals;
 
 class validateBinaryTreeNodes {
-public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-    int[] indegree = new int[n];
-    
-    for (int i = 0; i < n; i++) {
-        if (leftChild[i] != -1) {
-            indegree[leftChild[i]]++;
-            if (indegree[leftChild[i]] > 1) {
-                return false; // More than one incoming edge for a node.
-            }
-        }
-        if (rightChild[i] != -1) {
-            indegree[rightChild[i]]++;
-            if (indegree[rightChild[i]] > 1) {
-                return false; // More than one incoming edge for a node.
-            }
-        }
-    }
-    
-    int rootCount = 0;
-    
-    for (int i = 0; i < n; i++) {
-        if (indegree[i] == 0) {
-            rootCount++;
-            if (rootCount > 1) {
-                return false; // Multiple nodes with no incoming edges.
-            }
-        }
-    }
-    
-    if (rootCount == 0) {
-        return false; // No root found.
-    }
-    
-    // Ensure that the graph is connected and acyclic (no cycles).
-    boolean[] visited = new boolean[n];
-    if (!isConnectedAndAcyclic(0, leftChild, rightChild, visited)) {
-        return false;
-    }
-    
-    return true;
-}
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        int[] indegree = new int[n];
 
-private boolean isConnectedAndAcyclic(int node, int[] leftChild, int[] rightChild, boolean[] visited) {
-    if (node == -1) {
-        return true; // Null node is acyclic.
+        for (int i = 0; i < n; i++) {
+            if (leftChild[i] != -1) {
+                indegree[leftChild[i]]++;
+                if (indegree[leftChild[i]] > 1) {
+                    return false; // More than one incoming edge for a node.
+                }
+            }
+            if (rightChild[i] != -1) {
+                indegree[rightChild[i]]++;
+                if (indegree[rightChild[i]] > 1) {
+                    return false; // More than one incoming edge for a node.
+                }
+            }
+        }
+
+        int rootCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                rootCount++;
+                if (rootCount > 1) {
+                    return false; // Multiple nodes with no incoming edges.
+                }
+            }
+        }
+
+        if (rootCount == 0) {
+            return false; // No root found.
+        }
+
+        // Ensure that the graph is connected and acyclic (no cycles).
+        boolean[] visited = new boolean[n];
+        return isConnectedAndAcyclic(0, leftChild, rightChild, visited);
     }
-    
-    if (visited[node]) {
-        return false; // Cycle detected.
+
+    private boolean isConnectedAndAcyclic(int node, int[] leftChild, int[] rightChild, boolean[] visited) {
+        if (node == -1) {
+            return true; // Null node is acyclic.
+        }
+
+        if (visited[node]) {
+            return false; // Cycle detected.
+        }
+
+        visited[node] = true;
+        return isConnectedAndAcyclic(leftChild[node], leftChild, rightChild, visited)
+                && isConnectedAndAcyclic(rightChild[node], leftChild, rightChild, visited);
     }
-    
-    visited[node] = true;
-    return isConnectedAndAcyclic(leftChild[node], leftChild, rightChild, visited)
-        && isConnectedAndAcyclic(rightChild[node], leftChild, rightChild, visited);
-}
 
 }
